1ef1f74b675cd239dedecbc0833a03d5cf15b690
Manal Al-Subaei <alsubaeimanal@gmail.com>
2021-04-06 23:18:57 +0300

    Use some BS4 Navs lesson that I need

--- pages/HTML/header.php
+++ pages/HTML/header.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require "admin/dbconnect.php";?>
+require "admin/dbconnect.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,51 +18,58 @@ require "admin/dbconnect.php";?>
 </head>
 
 <body>
-
     <div class="navigation">
-        <a href="index.php" style="padding:0px; "><img src="../../images/log.png " alt="logo" width="auto" height="40"
-                align="left"
-                style="border-radius: 50px; top:0px; position:fixed; z-index:1; left:50px; object-fit: contain;"></a>
-        <ul>
-            <li class="li"><a href="index.php">Home</a></li>
-            <li class="li"><a href="signup.php">Sign up</a></li>
-            <li class="li"><a href="profile.php">Profile</a></li>
-            <li class="li"><a href="login.php">Login</a></li>
-            <li class="li"><a href="order.php">Order</a></li>
-            <li class="li">
+        <ul class="nav justify-content-center nav-pills nav-justified">
+            <li class="nav-item">
+                <a href="index.php" style="padding:0px;"  class="nav-link" data-toggle="pill"><img src="../../images/log.png " alt="logo" width="100%" height="40" ></a>
+            </li>
+            <li class="nav-item">
+                <a href="index.php" class="nav-link" data-toggle="pill">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="signup.php" class="nav-link" data-toggle="pill">Sign up</a>
+            </li>
+            <li class="nav-item">
+                <a href="profile.php" class="nav-link" data-toggle="pill">Profile</a>
+            </li>
+            <li class="nav-item">
+                <a href="login.php" class="nav-link" data-toggle="pill">Login</a>
+            </li>
+            <li class="nav-item">
+                <a href="order.php" class="nav-link" data-toggle="pill">Order</a>
+            </li>
+            <li class="nav-item">
                 <div class="dropdown menu">
-                    <a href="menu.php">Menu</a>
+                    <a href="menu.php" class="nav-link" data-toggle="pill">Menu</a>
                     <div class="Dropdown_content">
                         <ul>
-                            <?php 
-										$query="select * from category";
-										$stm=$connection->prepare($query);
-										$stm->execute();
-										if($stm->rowCount())
-										{
-											foreach($stm->fetchAll() as $row)
-											{
-													$id=$row['Category_id'];
-													$name=$row['Category_name'];
-													$image=$row['image'];
-													if( $name !== "Foods")
-													{
-									?>
-                            <li><a href="menu.php?C_id=<?php echo $id ?>"><?php echo $name ?></a></li>
-                            <?php	
-													}
-												else
-													{
-														?>
-                            <div class="dropdown_a">
-                                <li><a href="menu.php?C_id=<?php echo $id ?>"><?php echo $name ?></a>
+                            <?php
+                            $query = "select * from category";
+                            $stm = $connection->prepare($query);
+                            $stm->execute();
+                            if ($stm->rowCount()) {
+                                foreach ($stm->fetchAll() as $row) {
+                                    $id = $row['Category_id'];
+                                    $name = $row['Category_name'];
+                                    $image = $row['image'];
+                                    if ($name !== "Foods") {
+                            ?>
+                                        <li class="nav-item"><a class="nav-link" href="menu.php?C_id=<?php echo $id ?>"><?php echo $name ?></a></li>
                                     <?php
-											 		}
-											}
-										}
-									?>
+                                    } else {
+                                    ?>
+                                        <div class="dropdown_a">
+                                            <li class="nav-item"><a class="nav-link" href="menu.php?C_id=<?php echo $id ?>"><?php echo $name ?></a>
+                                    <?php
+                                    }
+                                }
+                            }
+                                    ?>
+                                        </div>
                     </div>
-                </div>
+            </li>
+            <li class="nav-item">
+                <a href="order.php"  style="padding:0px;"  class="nav-link" data-toggle="pill"><img src="../../images/trolley.png" alt="trolley" width="35" height="40"></a>
             </li>
         </ul>
     </div>
@@ -70,6 +77,4 @@ require "admin/dbconnect.php";?>
     </li>
 
     </ul>
-    <a href="order.php"><img src="../../images/trolley.png" alt="trolley" width="35" height="40" align="right"
-            style="top:0px; position:fixed; z-index:1; right:50px;"></a>
     </div>
