e4b032c15c2c86f78462af773bbd16eb5d2b799a
Manal Al-Subaei <alsubaeimanal@gmail.com>
2021-06-28 06:49:35 +0300

    Add logout page

---
+++ Logout.php
@@ -0,0 +1,12 @@
+	<?php include "header.php";
+	session_unset();
+	session_destroy();
+	?>
+	<br>
+	<br>
+	<br>
+	<h1 class="text-center">Success of Logout!</h1>
+	<br>
+	<?php
+	include "footer.php";
+	?>
